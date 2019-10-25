f94dcd3a1df70672919bcde8b1046e695b87f773
Smilencer <qianchenemail@gmail.com>
2019-10-25 16:21:21 +0800

    后台管理系统

--- static/js/doctor.js
+++ static/js/doctor.js
@@ -6,4 +6,29 @@ $(document).ready(function () {
         infinite: true,
         timeToRelaunch: 3000
     });
+    var sketchpad_1 = new Sketchpad({
+        element: "#canvas1",
+        width: 300,
+        height: 300,
+        readOnly: true
+    });
+    var sketchpad_2 = new Sketchpad({
+        element: "#canvas2",
+        width: 300,
+        height: 300,
+        readOnly: true
+    });
+    var sketchpad_3 = new Sketchpad({
+        element: "#canvas3",
+        width: 300,
+        height: 300,
+        readOnly: true
+    });
 });
+
+function showPad(padid, obj) {
+    $(".subpad").hide();
+    $(`#${padid}`).show();
+    $(".menu-selected").removeClass();
+    $(obj).addClass("menu-selected");
+}
